fbc9d21929b754a087d61559b0f1c7c1e4956581
Jorge Raze <jorge.raze@clevertech.biz>
2016-10-15 02:05:28 -0500

    Update db.rb

--- geoAPI/db.rb
+++ geoAPI/db.rb
@@ -1,3 +1,3 @@
 class dbPG
 	def getConnect
-		conn = PG.connect(dbname: 'gistmx', user: 'postgres', password: 'Razeone%88', host: '127.0.0.1')
+		conn = PG.connect(dbname: 'gistmx', user: 'postgres', password: '', host: '127.0.0.1')
